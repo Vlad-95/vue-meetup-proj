ba4dbb3668f1b3f65e136a6892aed05ea733c24d
vlad_z <zvezdinvladislav@yandex.ru>
2023-11-18 14:46:37 +0300

    Revert "wip"
    This reverts commit a88cb24548f53522ca8dd5c2e3540d576b5e4a9c.

--- src/components/UiInput.vue
+++ src/components/UiInput.vue
@@ -1,108 +1,12 @@
 <template>
-  <div class="form-group form-group_inline">
-    <!-- Task 06-wrappers/03-UiInput -->
-    <label>
-      <div
-        class="input-group"
-        :class="[
-          {
-            'input-group_icon': this.hasIcon(),
-            'input-group_icon-left': this.hasLeftIcon(),
-            'input-group_icon-right': this.hasRightIcon(),
-          },
-          $attrs.class,
-        ]"
-      >
-        <div v-if="hasLeftIcon()" class="input-group__icon">
-          <slot name="left-icon" />
-        </div>
-
-        <component
-          :is="tag"
-          ref="input"
-          class="form-control"
-          v-bind="{ ...$attrs, class: inputClasses }"
-          :value="modelValue"
-          @input="$emit('update:modelValue', $event.target.value)"
-        />
-
-        <div v-if="hasRightIcon()" class="input-group__icon">
-          <slot name="right-icon" />
-        </div>
-      </div>
-    </label>
-  </div>
+  <div>Task 06-wrappers/03-UiInput</div>
 </template>
 
 <script>
-import { computed } from 'vue';
-
 // TODO: Task 06-wrappers/03-UiInput
 
 export default {
   name: 'UiInput',
-
-  inheritAttrs: false,
-  props: {
-    modelValue: String,
-
-    small: {
-      type: Boolean,
-    },
-
-    rounded: {
-      type: Boolean,
-    },
-
-    multiline: {
-      type: Boolean,
-    },
-  },
-
-  emits: ['update:modelValue'],
-
-  setup(props, { emit }) {
-    // Computed
-    const tag = computed(() => {
-      return props.multiline ? 'textarea' : 'input';
-    });
-
-    const inputClasses = computed(() => {
-      return {
-        'form-control_rounded': props.rounded,
-        'form-control_sm': props.small,
-      };
-    });
-
-    // Methods
-    function focus() {
-      this.$refs.input.focus();
-    }
-
-    function hasIcon() {
-      return this.hasLeftIcon() || this.hasRightIcon();
-    }
-
-    function hasRightIcon() {
-      return !!this.$slots['right-icon'];
-    }
-
-    function hasLeftIcon() {
-      return !!this.$slots['left-icon'];
-    }
-
-    // emits
-    emit('update:modelValue');
-
-    return {
-      tag,
-      inputClasses,
-      focus,
-      hasIcon,
-      hasRightIcon,
-      hasLeftIcon,
-    };
-  },
 };
 </script>
 
@@ -174,13 +78,11 @@ textarea.form-control {
   transform: translate(0, -50%);
 }
 
-.input-group.input-group_icon.input-group_icon-left
-  .input-group__icon:first-child {
+.input-group.input-group_icon.input-group_icon-left .input-group__icon:first-child {
   left: 16px;
 }
 
-.input-group.input-group_icon.input-group_icon-right
-  .input-group__icon:last-child {
+.input-group.input-group_icon.input-group_icon-right .input-group__icon:last-child {
   right: 16px;
 }
 </style>
